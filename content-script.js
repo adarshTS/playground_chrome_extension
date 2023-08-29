0f7ead06d2e1afe583241c0a15397ed80f240570
adarshTS <adarsh.ts8@gmail.com>
2023-08-29 12:09:22 +0530

    added a space for formats

--- content-script.js
+++ content-script.js
@@ -1,6 +1,13 @@
-window.onload = () => {
+// window.onload = () => {
+//   console.log(
+//     "%cHello from Content-Script",
+//     "color:#fff; background:red; font-size:30px;"
+//   );
+// };
+
+window.onload = function () {
   console.log(
-    "%cHello from Content-Script",
-    "color:#fff; background:red; font-size:30px;"
+    "%cHello from your Chrome extension!",
+    "color: #fff; background: red; font-size: 20px; padding: 5px;"
   );
 };
